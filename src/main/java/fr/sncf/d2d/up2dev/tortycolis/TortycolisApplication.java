2c991c8e11b26fd0e2e55cc8ab57f7470705a552
N'ZAOU Renaud <r.nzaou@sncf.fr>
2023-07-03 14:59:34 +0200

    01

---
+++ src/main/java/fr/sncf/d2d/up2dev/tortycolis/TortycolisApplication.java
@@ -0,0 +1,13 @@
+package fr.sncf.d2d.up2dev.tortycolis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TortycolisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TortycolisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/sncf/d2d/up2dev/tortycolis/TortycolisApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.sncf.d2d.up2dev.tortycolis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TortycolisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
